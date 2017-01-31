390dccbe261f8f0a57f5ecb5c564241bdfd08974
Raman Nanda <raman148@gmail.com>
2017-01-31 11:12:28 -0500

    Adding Sample JS

---
+++ activities/example1.js
@@ -0,0 +1,38 @@
+var candy = [
+	{
+		"name": "Hershey",
+		"Nutrition Facts": {
+			"Calories": 360,
+			"Sugar": "41g",
+			"Sodium": "60mg",
+			"Protein": "6g"
+		}	
+	},
+	{
+		"name": "Banana",
+		"Nutrition Facts": {
+			"Calories": 105,
+			"Sugar": "14g",
+			"Sodium": "1mg",
+			"Protein": "1g"
+		}
+	},
+	{
+		"name": "Twix",
+		"Nutrition Facts": {
+			"Calories": 217,
+			"Sugar": "14.3g",
+			"Sodium": "14.3g",
+			"Protein": "3.3g"
+		}
+	},
+	{
+		"name": "KitKat",
+		"Nutrition Facts": {
+			"Calories": 286,
+			"Sugar": "29g",
+			"Sodium": "35g",
+			"Protein": "3.4g"
+		}
+	},
+]
---
+++ activities/example2.js
@@ -0,0 +1,34 @@
+var candy = {
+	"Hershey": {
+		"Nutrition Facts": {
+			"Calories": 360,
+			"Sugar": "41g",
+			"Sodium": "60mg",
+			"Protein": "6g"
+		}	
+	},
+	"Banana": {
+		"Nutrition Facts": {
+			"Calories": 105,
+			"Sugar": "14g",
+			"Sodium": "1mg",
+			"Protein": "1g"
+		}	
+	},
+	"Twix": {
+		"Nutrition Facts": {
+			"Calories": 217,
+			"Sugar": "14.3g",
+			"Sodium": "14.3g",
+			"Protein": "3.3g"
+		}	
+	},
+	"KitKat": {
+		"Nutrition Facts": {
+			"Calories": 286,
+			"Sugar": "29g",
+			"Sodium": "35g",
+			"Protein": "3.4g"
+		}
+	},	
+};
